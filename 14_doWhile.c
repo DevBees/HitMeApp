b270ab91316bee31880436cc6dec24d2bf6f9bf0
Govind B Chandran <govindchandran235@gmail.com>
2020-10-23 12:26:13 +0530

    Update 14_doWhile.c

--- 14_doWhile.c
+++ 14_doWhile.c
@@ -2,15 +2,12 @@
 
 int main () {
 	
-    // Isi i dengan nilai 0
 	int i = 0;
 	
-    // Lakukan perintah
 	do
 	{
-		// Cetak nilai i
 		printf("%d\n", i);
 		i++;
 	}
-	while (i < 0); // Selama i kurang dari 0
+	while (i < 0); 
 }

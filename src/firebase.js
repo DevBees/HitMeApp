944adfa61a2999c726dcec7a14500374c8cdd9ac
Govind B Chandran <govindchandran235@gmail.com>
2021-03-19 21:15:09 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpuTUZe5CtcXlPN-_cHEsZYahVZ-ffFVs",
+    authDomain: "hitmeapp-4346e.firebaseapp.com",
+    projectId: "hitmeapp-4346e",
+    storageBucket: "hitmeapp-4346e.appspot.com",
+    messagingSenderId: "1098359495362",
+    appId: "1:1098359495362:web:2f12342279db97b9e7f95b",
+    measurementId: "G-83V1291TRJ"
+  };

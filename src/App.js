f7724e715f0006c7cb3a90912c7cd4fc5f021725
Govind B Chandran <govindchandran235@gmail.com>
2021-03-19 23:02:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './hitmeapplogo.png';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <img alt= "logo" src="src/hitmeapplogo.svg" />
+
+    <div className="app__body">
+
+    <Sidebar />
+
+    </div>
+      
     </div>
   );
 }
